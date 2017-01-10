f5ec666cf75f05ed3cdc7292ac4ffbe9f3cffe22
administrator <rabit1@mailinator.com>
2017-01-10 09:19:10 +0700

    2nd

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/graph')
 def graph_Example(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
 #    df = pd.read_csv('csv/BCHAIN-TRVOU-BitcoinUSDExchangeTradeVolume.csv', index_col='Date', parse_dates=True)
-    df = pd.read_csv('csv/test.csv', index_col='Date', parse_dates=True)
+    df = pd.read_csv('csv/test.csv', index_col='Date', parse_dates=False)
     dataSet = pandas_highcharts.core.serialize(df, render_to='my-chart', output_type='json')
     return render_template('graph.html', chart=dataSet)
 
